038d64d60f3448af3cbd6e43528f8d1f182988e3
sankalp-yadav-au17 <sankalp_ma@srmuniv.edu.in>
2023-01-27 03:01:48 +0530

    removed console.log()

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,6 @@ import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
-  console.log("h");
   return (
     <section>
       <Container fluid className="home-section" id="home">
